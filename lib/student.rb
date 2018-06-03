098833f20be23ad965216c7d04ee76692a70d2ea
Ashley Schultz <ashleyeschultz@gmail.com>
2018-06-03 23:46:06 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -15,8 +15,8 @@ class Student
   end
 
   def self.all
-    DB[:conn].execute("SELECT * FROM students").each {|row| new_from_db(row)}
-    @@all
+    all = DB[:conn].execute("SELECT * FROM students").each {|row| new_from_db(row)}
+    all
     # retrieve all the rows from the "Students" database
     # remember each row should be a new instance of the Student class
   end
