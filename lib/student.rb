ec2ec8152817329d1c2f7d3cbd88e46bd6483cfc
Ashley Schultz <ashleyeschultz@gmail.com>
2018-06-03 23:31:22 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -9,8 +9,6 @@ class Student
     o.id = row[0]
     o.name = row[1]
     o.grade = row[2]
-    binding.pry
-    #@@all << self
     # create a new Student object given a row from the database
   end
 
