8fb8ece8c81a05cc7423d67a9dc05df3cbf6a8ca
Ashley Schultz <ashleyeschultz@gmail.com>
2018-06-03 23:41:53 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -26,7 +26,7 @@ class Student
   def self.find_by_name(name)
     row = DB[:conn].execute("SELECT * FROM students WHERE name = ?", name).flatten
     student = new_from_db(row)
-    binding.pry
+    student
     # find the student in the database given a name
     # return a new instance of the Student class
   end
