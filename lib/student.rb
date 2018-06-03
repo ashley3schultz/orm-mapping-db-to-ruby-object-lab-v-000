c4df5402c2b5e1df080e566903645a2cdcf6ca15
Ashley Schultz <ashleyeschultz@gmail.com>
2018-06-03 23:29:53 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -32,7 +32,7 @@ class Student
     row = DB[:conn].execute(sql, name).flatten
     student = new_from_db(row)
     student
-    binding.pry
+    #binding.pry
     # find the student in the database given a name
     # return a new instance of the Student class
   end
